1da2f125b2a85a128d0c22e1faed64b36b8039fb
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-10-13 04:38:35 +1100

    init nextjs project + hero section (landing page)

---
+++ app/_components/HeroSection.tsx
@@ -0,0 +1,39 @@
+const HeroSection = () => {
+  return (
+    <div className="flex items-center justify-around w-full max-w-7xl mx-auto px-5 gap-x-10">
+      <div className="flex flex-col gap-y-3 text-center max-w-lg z-20">
+        <div
+          className="flex justify-center items-center text-5xl sm:text-7xl
+          text-transparent font-extrabold gap-x-2 sm:gap-x-3"
+        >
+          <h1 className="bg-gradient-to-r from-[#217bfe] to-[#e55571] bg-clip-text">
+            UniPath
+          </h1>
+          <h1 className="bg-gradient-to-r from-[#217bfe] to-[#e55571] bg-clip-text">
+            UNSW
+          </h1>
+        </div>
+
+        <h2 className="text-xl lg:text-2xl font-medium">
+          Course Recommendation Chatbot
+          <br />
+          for UNSW students
+        </h2>
+
+        <h3 className="text-neutral-300">
+          Are you feeling confused and overwhelmed by the choices you have?
+          <br /> Not knowing which course to take for your career?
+        </h3>
+
+        <button
+          className="bg-gradient-to-r from-[#217bfe] to-[#e55571] px-6 py-3
+          rounded-full w-auto font-semibold text-lg hover:opacity-90 transition-all"
+        >
+          Ask Here
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "@/app/_components/HeroSection";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen h-full flex justify-start bg-[#0e0c16] text-white">
+      <HeroSection />
+    </div>
+  );
+}
