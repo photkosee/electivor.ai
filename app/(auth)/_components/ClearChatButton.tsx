4442bb9d5847122cc2b9ab7ad04dbd1bea33807b
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-10-14 11:25:29 +1100

    add: confirm modal before clearing the chat

--- app/(auth)/_components/ClearChatButton.tsx
+++ app/(auth)/_components/ClearChatButton.tsx
@@ -7,13 +7,20 @@ import { db } from "@/app/firebase";
 import useMessageStore from "@/app/_stores/MessageStore";
 import useSidebarStore from "@/app/_stores/SidebarStore";
 import useUserStore from "@/app/_stores/UserStore";
+import Modal from "@/app/_components/Modal";
+import { useEffect, useRef, useState } from "react";
+import LoadingScreen from "@/app/_components/LoadingScreen";
 
 const ClearChatButton = () => {
   const { sidebarExpand } = useSidebarStore();
   const { setMessages } = useMessageStore();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const buttonSubmitRef = useRef<HTMLButtonElement>(null);
   const { email } = useUserStore();
 
   const clearChat = async () => {
+    setLoading(true);
     const userRef = doc(db, "users", email);
 
     try {
@@ -21,42 +28,98 @@ const ClearChatButton = () => {
         messages: [],
       });
       setMessages([]);
+      setTimeout(() => {
+        setLoading(false);
+        setOpen(false);
+      }, 500);
     } catch (error) {
-      console.error("Error clearing chat: ", error);
+      setTimeout(() => {
+        setLoading(false);
+        setOpen(false);
+      }, 500);
     }
   };
 
+  useEffect(() => {
+    if (open && buttonSubmitRef.current) {
+      setTimeout(() => {
+        buttonSubmitRef.current?.focus();
+      }, 50);
+    }
+  }, [open]);
+
   return (
-    <button
-      className="relative flex items-center py-2 px-3 my-1 font-medium rounded-md
-      transition-colors group hover:bg-red-50 text-red-500"
-      onClick={clearChat}
-    >
-      <div className="flex-shrink-0">
-        <MessageSquareX />
-      </div>
-
-      <span
-        className={`overflow-hidden transition-all text-nowrap text-start ${
-          sidebarExpand ? "w-52 ml-3" : "w-0"
-        }`}
+    <>
+      <button
+        className="relative flex items-center py-2 px-3 my-1 font-medium rounded-md
+        transition-colors group hover:bg-red-50 text-red-500"
+        onClick={() => setOpen(true)}
       >
-        Clear Chat
-      </span>
+        <div className="flex-shrink-0">
+          <MessageSquareX />
+        </div>
 
-      {!sidebarExpand && (
-        <div
-          className={`
+        <span
+          className={`overflow-hidden transition-all text-nowrap text-start ${
+            sidebarExpand ? "w-52 ml-3" : "w-0"
+          }`}
+        >
+          Clear Chat
+        </span>
+
+        {!sidebarExpand && (
+          <div
+            className={`
             absolute left-full rounded-md px-2 py-1 ml-6
             bg-red-100 text-red-800 text-sm
             invisible opacity-20 -translate-x-3 transition-all
             group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
           `}
-        >
-          Clear Chat
-        </div>
-      )}
-    </button>
+          >
+            Clear Chat
+          </div>
+        )}
+      </button>
+
+      <Modal open={open} onClose={() => setOpen(false)}>
+        {loading ? (
+          <div className="p-10">
+            <LoadingScreen />
+          </div>
+        ) : (
+          <form
+            className="select-none flex flex-col gap-y-5 py-6"
+            onSubmit={clearChat}
+          >
+            <div className="mx-auto text-center">
+              <h3 className="text-lg font-black text-gray-800">
+                Clear All Messages
+              </h3>
+            </div>
+
+            <div className="flex gap-x-2">
+              <button
+                className="w-full hover:bg-red-500 transition-all px-3 py-2 rounded-xl
+                bg-red-600 text-white"
+                type="submit"
+                autoFocus
+                ref={buttonSubmitRef}
+              >
+                Clear
+              </button>
+
+              <button
+                className="w-full hover:bg-neutral-100 transition-all px-3 py-2 rounded-xl"
+                onClick={() => setOpen(false)}
+                type="button"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        )}
+      </Modal>
+    </>
   );
 };
 
